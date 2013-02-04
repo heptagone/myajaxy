575979c31eb2852b29d40c472a0af6b0ef7fa2e1
heptagone <heptagone@dec.(none)>
2013-02-04 10:49:28 -0500

    rails_apps_composer: initial commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Myajaxy::Application.config.secret_token = '3045a09fb1d80e292e2ea8fd8004c9ef29b0f5eebf8d6d25a136e72d4753707c79757e99cf05510137e3839c7f0a4b5edb3472094ab0e4d01a523f386f8249e6'
+Myajaxy::Application.config.secret_token = 'a513d09b7dd6722c78cab8992079347f1fd89fe37c6105da83e9f635f69f28d2dd141ff138cbcc8b58be70c06f228c15cb2e8b67ed2e04f25e96674e88f48fb2'
